1c8b13a931f1f0dad9da6c8bfb36026cac6249c3
samblaeser <sblaes1@lsu.edu>
2019-08-29 13:51:30 -0500

    my first assignment commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+createCanvas(800, 600);
+background(100, 255, 100);
+
+    fill(0, 0, 0);
+    
+    ellipse(width/2, height/2, 100, 100);
+
+   // ellipse(100, 300, 100, 100);
+
+}
+function draw () {
+}
