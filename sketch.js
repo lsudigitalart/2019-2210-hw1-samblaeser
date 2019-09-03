d25d4ba3ed1f4b6b8d7e8a3226d66ae9536a2da3
samblaeser <sblaes1@lsu.edu>
2019-09-03 07:34:46 -0500

    added one little thing to each cat final

--- sketch.js
+++ sketch.js
@@ -1,13 +1,623 @@
 function setup() {
-createCanvas(800, 600);
-background(100, 255, 100);
+createCanvas(2400, 600);
+background(230);
 
-    fill(0, 0, 0);
+//cheetah
+fill(153);
+strokeWeight(.25);
+stroke(120);
+ellipse(400, 650, 350, 950);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(420, 585, 45, 45);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(350, 585, 35, 35);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(500, 525, 55, 50);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(250, 535, 40, 40);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(300, 546, 25, 25);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(450, 565, 22, 23);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(249, 575, 22, 23);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(289, 500, 19, 19);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(550, 590, 45, 45);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(390, 537, 55, 55);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(297, 597, 65, 65);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(255, 485, 15, 15);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(340, 538, 15, 15);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(555, 485, 15, 15);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(505, 575, 30, 30);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(485, 597, 25, 25);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(545, 535, 30, 30);
+
+fill(163);
+strokeWeight(.25);
+stroke(100);
+ellipse(240, 160, 100, 100);
+
+fill(163);
+strokeWeight(.25);
+stroke(100);
+ellipse(560, 160, 100, 100);
+
+fill(220);
+strokeWeight(.25);
+stroke(210);
+ellipse(545, 170, 50, 50);
+
+fill(220);
+strokeWeight(.25);
+stroke(210);
+ellipse(255, 170, 50, 50);
+
+fill(70);
+strokeWeight(.25);
+stroke(40);
+ellipse(255, 170, 40, 40);
+
+fill(70);
+strokeWeight(.25);
+stroke(40);
+ellipse(545, 170, 40, 40);
+
+fill(163);
+//strokeWeight(12);
+//stroke(255);
+ellipse(400, 320, 400, 400);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(575, 300, 25, 25);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(545, 390, 29, 29);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(505, 440, 25, 25);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(274, 460, 22, 22);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(341, 402, 22, 22);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(239, 360, 30, 30);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(270, 416, 20, 20);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(455, 440, 15, 15);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(397, 320, 22, 22);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(417, 340, 12, 12);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(403, 380, 30, 30);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(257, 260, 23, 23);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(312, 430, 18, 18);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(456, 407, 21, 21);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(581, 360, 23, 23);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(247, 210, 20, 20);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(222, 300, 19, 19);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(507, 220, 45, 45);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(417, 200, 40, 40);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(337, 160, 50, 50);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(347, 210, 22, 22);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(289, 206, 30, 30);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(537, 260, 20, 20);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(427, 260, 22, 22);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(362, 250, 19, 19);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(447, 150, 42, 42);
+
+fill(60);
+strokeWeight(.25);
+stroke(60);
+ellipse(497, 180, 30, 30);
+
+fill(176);
+strokeWeight(.25);
+stroke(176);
+ellipse(400, 500, 170, 90);
+
+fill(176);
+strokeWeight(.25);
+stroke(176);
+ellipse(400, 460, 90, 100);
+
+fill(50);
+strokeWeight(.25);
+stroke(50);
+ellipse(400, 455, 60, 36);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(380, 460, 20, 32);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(420, 460, 20, 32);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(400, 460, 17, 32);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(442, 490, 5, 5);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(358, 490, 5, 5);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(460, 506, 5, 5);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(340, 506, 5, 5);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(358, 521, 5, 5);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(442, 521, 5, 5);
+
+fill(60);
+strokeWeight(.25);
+stroke(50);
+ellipse(490, 321, 120, 130);
+
+fill(60);
+strokeWeight(.25);
+stroke(50);
+ellipse(310, 321, 120, 130);
+
+fill(255, 255, 0);
+strokeWeight(.5);
+stroke(255, 200, 0);
+ellipse(490, 321, 90, 120);
+
+fill(255, 255, 0);
+strokeWeight(.5);
+stroke(255, 200, 0);
+ellipse(310, 321, 90, 120);
+
+fill(25, 25, 0);
+strokeWeight(.25);
+stroke(25, 20, 0);
+ellipse(490, 321, 20, 60);
+
+fill(25, 25, 0);
+strokeWeight(.25);
+stroke(25, 20, 0);
+ellipse(310, 321, 20, 60);
+
+
+
+
+//lion
+    fill(123);
+    strokeWeight(.25);
+    stroke(100);
+    ellipse(1200, 650, 350, 950);
+
+    fill(80);
+    strokeWeight(.25);
+    stroke(80);
+    ellipse(1200, 300, 550, 550);
+
+    fill(130);
+    strokeWeight(.25);
+    stroke(100);
+    ellipse(1040, 160, 100, 100);
+
+    fill(130);
+    strokeWeight(.25);
+    stroke(100);
+    ellipse(1360, 160, 100, 100);
+
+    fill(160);
+    strokeWeight(.25);
+    stroke(100);
+    ellipse(1055, 170, 40, 40);
+
+    fill(160);
+    strokeWeight(.25);
+    stroke(100);
+    ellipse(1345, 170, 40, 40);
+
+    fill(80);
+    strokeWeight(.25);
+    stroke(80);
+    ellipse(1200, 300, 400, 400);
+
+    fill(130);
+    //strokeWeight(12);
+    //stroke(255);
+    ellipse(1200, 320, 400, 400);
+
+    fill(160);
+    strokeWeight(.25);
+    stroke(140);
+    ellipse(1200, 500, 170, 90);
+
+    fill(160);
+    strokeWeight(.25);
+    stroke(160);
+    ellipse(1200, 460, 90, 160);
+
+    fill(50);
+    strokeWeight(.25);
+    stroke(50);
+    ellipse(1200, 455, 60, 36);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1180, 460, 20, 32);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1220, 460, 20, 32);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1200, 460, 17, 32);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1242, 490, 5, 5);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1158, 490, 5, 5);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1260, 506, 5, 5);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1140, 506, 5, 5);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1158, 521, 5, 5);
+
+        fill(50);
+        strokeWeight(.25);
+        stroke(50);
+        ellipse(1242, 521, 5, 5);
+
+    fill(160);
+    strokeWeight(.25);
+    stroke(160);
+    ellipse(1290, 321, 130, 150);
+
+    fill(160);
+    strokeWeight(.25);
+    stroke(160);
+    ellipse(1110, 321, 130, 150);
+
+    fill(255, 255, 0);
+    strokeWeight(.5);
+    stroke(255, 200, 0);
+    ellipse(1290, 321, 90, 120);
     
-    ellipse(width/2, height/2, 100, 100);
+    fill(255, 255, 0);
+    strokeWeight(.5);
+    stroke(255, 200, 0);
+    ellipse(1110, 321, 90, 120);
 
-   // ellipse(100, 300, 100, 100);
+    fill(25, 25, 0);
+    strokeWeight(.25);
+    stroke(25, 20, 0);
+    ellipse(1290, 321, 20, 60);
 
+    fill(25, 25, 0);
+    strokeWeight(.25);
+    stroke(25, 20, 0);
+    ellipse(1110, 321, 20, 60);
+
+
+
+
+//panther
+fill(47);
+strokeWeight(.25);
+stroke(30);
+ellipse(2000, 650, 350, 950);
+
+fill(80);
+strokeWeight(.25);
+stroke(50);
+ellipse(2000, 650, 200, 950);
+
+fill(50);
+strokeWeight(.25);
+stroke(55);
+ellipse(1840, 160, 100, 100);
+
+fill(50);
+strokeWeight(.25);
+stroke(55);
+ellipse(2160, 160, 100, 100);
+
+fill(90);
+strokeWeight(.25);
+stroke(90);
+ellipse(1855, 170, 40, 40);
+
+fill(90);
+strokeWeight(.25);
+stroke(90);
+ellipse(2145, 170, 40, 40);
+
+fill(50);
+strokeWeight(.5);
+stroke(45);
+ellipse(2000, 320, 400, 400);
+
+fill(90);
+strokeWeight(.25);
+stroke(90);
+ellipse(2000, 500, 170, 90);
+
+fill(90);
+strokeWeight(.25);
+stroke(90);
+ellipse(2000, 450, 90, 180);
+
+fill(40);
+strokeWeight(.25);
+stroke(40);
+ellipse(2000, 455, 60, 36);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(1980, 460, 20, 32);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(2020, 460, 20, 32);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(2000, 460, 17, 32);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(2042, 490, 5, 5);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(1958, 490, 5, 5);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(2060, 506, 5, 5);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(1940, 506, 5, 5);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(1958, 521, 5, 5);
+
+    fill(40);
+    strokeWeight(.25);
+    stroke(40);
+    ellipse(2042, 521, 5, 5);
+
+fill(90);
+strokeWeight(.25);
+stroke(70);
+ellipse(2090, 324, 120, 140);
+
+fill(90);
+strokeWeight(.25);
+stroke(70);
+ellipse(1910, 324, 120, 140);
+
+fill(255, 255, 0);
+strokeWeight(.5);
+stroke(255, 200, 0);
+ellipse(2090, 321, 90, 120);
+
+fill(255, 255, 0);
+strokeWeight(.5);
+stroke(255, 200, 0);
+ellipse(1910, 321, 90, 120);
+
+fill(25, 25, 0);
+strokeWeight(.25);
+stroke(25, 20, 0);
+ellipse(2090, 321, 20, 60);
+
+fill(25, 25, 0);
+strokeWeight(.25);
+stroke(25, 20, 0);
+ellipse(1910, 321, 20, 60);
+
+
+    
 }
 function draw () {
-}
+} 
